8616208c8407ae496d3e79b1035d8dc7e8979c59
arakelyanva <vahan9797@gmail.com>
2026-05-14 01:02:49 +0400

    Fix runner download link for Hetzner VPS

--- scripts/deploy_runner.sh
+++ scripts/deploy_runner.sh
@@ -8,6 +8,7 @@ RUNNER_REGISTRATION_TOKEN="${3}"
 REPO_URL="${4}"
 
 cat << EOF > cloud-config.yaml
+#cloud-config
 runcmd:
   # Firewall setup
   - ufw default deny incoming
@@ -22,8 +23,9 @@ runcmd:
 
   # GHA self-hosted setup
   - mkdir /actions-runner && cd /actions-runner
-  - curl -o actions-runner-linux-x64-2.316.1.tar.gz -L github.com
-  - tar xzf ./actions-runner-linux-x64-2.316.1.tar.gz
+  - curl -o actions-runner-linux-x64-2.334.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.334.0/actions-runner-linux-x64-2.334.0.tar.gz
+  - tar xzf ./actions-runner-linux-x64-2.334.0.tar.gz
+  - useradd -m -s /bin/bash ubuntu
   - chown -R ubuntu:ubuntu /actions-runner
   - sudo -u ubuntu ./config.sh --url "${REPO_URL}" --token "${RUNNER_REGISTRATION_TOKEN}" --name "hetzner-isolated-node" --labels "hetzner-vps" --ephemeral --unattended
   - sudo -u ubuntu ./run.sh
